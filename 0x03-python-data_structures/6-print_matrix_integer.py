db6be3f68daca5e525f8257598c28404ff1704bc
yvesmfura <132387277+yvesmfura@users.noreply.github.com>
2023-12-04 21:22:54 +0200

    printing matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# printing matrix
+def print_matrix_integer(matrix):
+    for row in matrix:
+        for num in row:
+            print("{:d}".format(num), end=" ")
+        print()
